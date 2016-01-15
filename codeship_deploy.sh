387e990bdb6c548f21b81afeb0b2d867d77acadc
Matias Meno <m@tias.me>
2016-01-15 23:27:46 +0100

    feat: add deployment

--- codeship_deploy.sh
+++ codeship_deploy.sh
@@ -2,3 +2,21 @@
 
 pub build
 
+deploymentDir=deployment
+
+git clone git@github.com:enyo/vocal-coach.git $deploymentDir
+
+cd $deploymentDir
+
+git checkout gh-pages
+
+rm -r ./*
+
+mv ../build/* .
+
+git add .
+git add -u
+
+git commit -m 'New release'
+
+git push
