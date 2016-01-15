2d07645f17dd1a27d90a20240482a7e7c0274d6a
Matias Meno <m@tias.me>
2016-01-15 23:32:27 +0100

    chore: improve deploy script

--- codeship_deploy.sh
+++ codeship_deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Abort on error
+set -e
+
 pub build
 
 deploymentDir=deployment
@@ -8,15 +11,17 @@ git clone git@github.com:enyo/vocal-coach.git $deploymentDir
 
 cd $deploymentDir
 
+git config --global user.email "m@tias.me"
+git config --global user.name "Matias Meno"
+
 git checkout gh-pages
 
 rm -r ./*
 
 mv ../build/* .
 
-git add .
-git add -u
+git add --all .
 
 git commit -m 'New release'
 
-git push
+git push origin gh-pages
